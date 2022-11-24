543864dfef7319e02ce2fd64d4154688e4c5a31a
Elin Asplund <elin.asplund91@gmail.com>
2022-11-24 22:38:43 +0100

    new product saved to api

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,61 +2,93 @@ const express = require('express')
 const controller = express.Router()
 let products = require('../data/simulated_database')
 
-// // middleware
-// controller.param('id', (req, res, next, id) => {
-//     req.user = users.find(user => user.id == id)
-//     next()
-// })
+// middleware
+controller.param('id', (req, res, next, id) => {
+    req.product = products.find(product => product.id == id)
+    next()
+})
 
 
-// // POST - CREATE - SKAPA EN ANVÄNDARE - http://localhost:5000/api/users
-// controller.post('/', (httpRequest, httpResponse) => {
-//     let user = {
-//         id: (users[users.length-1])?.id > 0 ? (users[users.length-1])?.id + 1 : 1,
-//         firstName: httpRequest.body.firstName,
-//         lastName: httpRequest.body.lastName,
-//         email: httpRequest.body.email,
-//         password: httpRequest.body.password
-//     }
+// POST - CREATE - SKAPA EN PRODUKT - http://localhost:5000/api/products
+controller.post('/', (request, response) => {
+    let product = {
+        id: (products[products.length-1])?.id > 0 ? (products[products.length-1])?.id + 1 : 1,
+        articleNumber: request.body.articleNumber,
+        name: request.body.name,
+        description: request.body.description,
+        category: request.body.category,
+        price: request.body.price,
+        rating: request.body.rating,
+        imageName: request.body.imageName
+    }
 
-//     users.push(user)
-//     httpResponse.status(201).json(user)
-// })
+    products.push(product)
+    response.status(201).json(product)
+})
 
-// // GET - READ - HÄMTA ALLA ANVÄNDARE - http://localhost:5000/api/users
-// controller.get('/', (httpRequest, httpResponse) => {
-//     httpResponse.status(200).json(users)
-// })
+// GET - READ - HÄMTA ALLA PRODUKTER - http://localhost:5000/api/products
+controller.get('/', (request, response) => {
+    response.status(200).json(products)
+})
 
-// // http://localhost:5000/api/users/1
-// controller.route('/:id')
-// .get((httpRequest, httpResponse) => {
-//     if (httpRequest.user != undefined)
-//         httpResponse.status(200).json(httpRequest.user)
-//     else
-//         httpResponse.status(404).json()
-// })
-// .put((httpRequest, httpResponse) => {
-//     if (httpRequest.user != undefined){
-//         ursers.forEach(user => {
-//             if (user.id == httpRequest.user.id){
-//                 user.firstName = httpRequest.body.firstName ? httpRequest.body.firstName : user.firstName
-//                 user.lastName = httpRequest.body.lastName ? httpRequest.body.lastName : user.lastName
-//                 user.email = httpRequest.body.email ? httpRequest.body.email : user.email
-//             }
-//         })
-//         httpResponse.status(200).json(httpRequest.user)
-//     }
-//     else
-//         httpResponse.status(404).json()
-// })
-// .delete((httpRequest, httpResponse) => {
-//     if (httpRequest.user != undefined){
-//         users = users.filter(user => user.id !== httpRequest.user.id)
-//         httpResponse.status(204).json()
-//     }
-//     else
-//         httpResponse.status(404).json()
-// })
+// http://localhost:5000/api/products/1
+controller.route('/:id')
+.get((request, response) => {
+    if (request.product != undefined)
+        response.status(200).json(request.product)
+    else
+        response.status(404).json()
+})
+.put((request, response) => {
+    if (request.product != undefined){
+        ursers.forEach(product => {
+            if (product.id == request.product.id){
+                product.articleNumber = request.body.articleNumber ? request.body.articleNumber : product.articleNumber
+                product.name = request.body.name ? request.body.name : product.name
+                product.description = request.body.description ? request.body.description : product.description
+                product.category = request.body.category ? request.body.category : product.category
+                product.price = request.body.price ? request.body.price : product.price
+                product.rating = request.body.rating ? request.body.rating : product.rating
+                product.imageName = request.body.imageName ? request.body.imageName : product.imageName
+            }
+        })
+        response.status(200).json(request.user)
+    }
+    else
+        response.status(404).json()
+})
+.delete((request, response) => {
+    if (request.user != undefined){
+        products = products.filter(product => product.id !== request.product.id)
+        response.status(204).json()
+    }
+    else
+        response.status(404).json()
+})
 
 module.exports = controller
+
+
+// export interface Product {
+//     id?: number,
+//     articleNumber: string,
+//     name: string,
+//     description?: string,
+//     category: string,
+//     price: number,
+//     rating: number,
+//     imageName: string,
+//     initialPrice?: number
+//   }
+  
+//   // "NÄR VI SKAPAR EN PRODUKT"?
+//   export interface ProductRequest {
+//     articleNumber: string,
+//     name: string,
+//     description?: string,
+//     category: string,
+//     price: number,
+//     rating: number,
+//     imageName: string,
+//     initialPrice?: number
+//   }
