a29024615ab5a3cf7827f94cf72b4e6620b74f43
Elin Asplund <elin.asplund91@gmail.com>
2022-11-28 19:40:00 +0100

    added products

--- data/simulated_database.js
+++ data/simulated_database.js
@@ -1,2 +1,88 @@
-const products = []
+const products = [
+{
+    "id": 1,
+    "name": "Mordern Black Blouse",
+    "category": "Fashion",
+    "price": "35",
+    "imageName": "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 2,
+    "name": "Happy Sweatshirt",
+    "category": "Basic",
+    "price": 25,
+    "imageName": "https://images.unsplash.com/photo-1529068755536-a5ade0dcb4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTI5fHxwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 3,
+    "name": "Colorful Dress",
+    "category": "Fashion",
+    "price": "38",
+    "imageName": "https://images.unsplash.com/photo-1554727242-741c14fa561c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTMzfHxwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 4,
+    "name": "Jeans Jacket",
+    "category": "Jackets",
+    "price": 45,
+    "imageName": "https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjM0fHxwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 5,
+    "name": "Blue Basic T-shirt",
+    "category": "Basic",
+    "price": 15,
+    "imageName": "https://images.unsplash.com/photo-1519764622345-23439dd774f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTc4fHxwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 6,
+    "name": "Pattern Dress",
+    "category": "Fashion",
+    "price": 40,
+    "imageName": "https://images.unsplash.com/photo-1602414336756-1d5b26bbbdf5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjR8fGRyZXNzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 7,
+    "name": "Knitted Jumper",
+    "category": "Fashion",
+    "price": 35,
+    "imageName": "https://images.unsplash.com/photo-1506634572416-48cdfe530110?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzU4fHxwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 8,
+    "name": "Tank Top",
+    "category": "Basic",
+    "price": 20,
+    "imageName": "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjMyfHxwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 9,
+    "name": "Fine Hoodie",
+    "category": "Casual",
+    "price": 25,
+    "imageName": "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 10,
+    "name": "Nice Hat",
+    "category": "Accessories",
+    "price": 15,
+    "imageName": "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 11,
+    "name": "Extreme Pattern Shirt",
+    "category": "Fashion",
+    "price": 35,
+    "imageName": "https://images.unsplash.com/photo-1536766820879-059fec98ec0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjJ8fHNoaXJ0JTIwbWVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+},
+{
+    "id": 12,
+    "name": "Yellow Two Piece Set",
+    "category": "Casual",
+    "price": 40,
+    "imageName": "https://img.ltwebstatic.com/images3_pi/2021/04/28/1619582567d38c8f7365fcdaf04f591c7e6f3858e0.webp"
+}
+];
+
 module.exports = products
